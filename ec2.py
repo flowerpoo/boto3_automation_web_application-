b2726aca632bf010c77f952ca511f3e41c79ad0e
poovarasan <poovarasancgm@gmail.com>
2024-02-17 22:07:05 +0530

    lambda updation

--- ec2.py
+++ ec2.py
@@ -114,6 +114,9 @@ def create_EC2_instance():
 
 
 #Load balancing part 
+    
+# defining empty list to store the target arn's
+target_group_arns = []
 def create_alb():
     client = boto3.client('elbv2', region_name=region_name)
     
@@ -148,7 +151,8 @@ def create_alb():
     # Create target group
     target_group_response = client.create_target_group(**target_group_params)
     target_group_arn = target_group_response['TargetGroups'][0]['TargetGroupArn']
-    print("Target group created successfully:", target_group_arn)
+    target_group_arns.append(target_group_arn)
+    print("Target group created successfully:", target_group_arns)
     
     
 
@@ -180,6 +184,114 @@ def create_alb():
     print("Listener created successfully")
 
 #Function call to create ALB
-create_alb()
+#print(create_alb())
 
+
+#ASG configuration 
+#function to create auto scalling group
+asg_grp_arn=[]    
+def create_asg():
+    # Initialize AWS clients
+    ec2_client = boto3.client('ec2')
+    asg_client = boto3.client('autoscaling',region_name=region_name)
+    instance_ids = ['i-00a4d65741d135b58'] 
+    target_group_arns=['arn:aws:elasticloadbalancing:us-east-1:471112994703:targetgroup/my-target-group/e2866d138ec362bc']
+    
+
+    # Define auto scaling group parameters
+    asg_params = {
+        'AutoScalingGroupName': 'my-asg',  # Replace with your ASG name
+        #'LaunchConfigurationName': 'my-launch-config',  # Replace with your launch configuration name
+        'MinSize': 1,  # Replace with your minimum instance count
+        'MaxSize': 5,  # Replace with your maximum instance count
+        'DesiredCapacity': 2,  # Replace with your desired instance count
+        #'AvailabilityZones': ['us-west-1a', 'us-west-1b'],  # Replace with your availability zones
+        'InstanceId': instance_ids[0],
+        'TargetGroupARNs': target_group_arns
+        # Add additional parameters as needed
+    }
+
+    # Create auto scaling group
+    asg_response= asg_client.create_auto_scaling_group(**asg_params)
+    asg_arn = asg_response['AutoScalingGroups'][0]['AutoScalingGroupARN']
+    asg_grp_arn.append(asg_arn)
+    print("Auto Scaling Group created successfully ",asg_grp_arn)
+
+    # Define scaling policies
+    scaling_policy_params = {
+        'AutoScalingGroupName': 'my-asg',  # Replace with your ASG name
+        'PolicyName': 'my-scaling-policy',  # Replace with your scaling policy name
+        'ScalingAdjustment': 1,  # Replace with your scaling adjustment (e.g., 1 for scale out, -1 for scale in)
+        'Cooldown': 300,  # Replace with your cooldown period in seconds
+        'InstanceId': instance_ids[0],
+        'DesiredCapacity': 2,
+        'AdjustmentType': 'ChangeInCapacity',  # Replace with 'PercentChangeInCapacity' for percentage-based adjustment
+        # Add additional parameters as needed
+    }
+
+    # Create scale out policy
+    asg_client.put_scaling_policy(
+        **scaling_policy_params,
+        PolicyType='SimpleScaling',
+        AdjustmentType='ChangeInCapacity',
+        Cooldown=300,
+        ScalingAdjustment=1
+    )
+    print("Scale out policy created successfully")
+
+    # Create scale in policy
+    asg_client.put_scaling_policy(
+        **scaling_policy_params,
+        PolicyName='my-scaling-policy-scale-in',  # Replace with your scale in policy name
+        ScalingAdjustment=-1  # Replace with your scaling adjustment for scale in
+    )
+    print("Scale in policy created successfully")
+
+#function call for asg config
+#print(create_asg())
+
+
+#Loggs and monitors 
+
+s3_bucket_name = 'first-bucket-poo-web'
+elb_client = boto3.client('elbv2', region_name=region_name)    
+alb_arn = []
+alb_name='my-alb'
+
+response_describe_arn = elb_client.describe_load_balancers(
+    Names=[alb_name],
+)
+
+alb_arn.append(response_describe_arn['LoadBalancers'][0]['LoadBalancerArn'])
+
+
+def configure_alb_access_logs(alb_arn, s3_bucket_name):
+    # Initialize boto3 client
+    elbv2_client = boto3.client('elbv2')
+
+    # Configure ALB access logs
+    response = elbv2_client.modify_load_balancer_attributes(
+        LoadBalancerArn=alb_arn,
+        Attributes=[
+            {
+                'Key': 'access_logs.s3.enabled',
+                'Value': 'true'
+            },
+            {
+                'Key': 'access_logs.s3.bucket',
+                'Value': s3_bucket_name
+            }
+        ]
+    )
+
+    print('ALB access logs configured successfully.')
+
+
+# Configure ALB access logs function call
+configure_alb_access_logs(alb_arn, s3_bucket_name)
+
+bucket_name = 'log-bucket11'
+#function call to create a bucket 
+buc=create_s3_bucket(bucket_name)
+print(buc)
 
--- lambda.py
+++ lambda.py
@@ -6,25 +6,30 @@ def lambda_handler(event, context):
 
     # Create ELB client
     elbv2 = boto3.client('elbv2', region_name=region)
+    ec2 = boto3.client('ec2', region_name=region)
+    sns = boto3.client('sns', region_name=region)
+    asg = boto3.client('autoscaling', region_name=region)
 
-    """# Describe all load balancers
-    response = elbv2.describe_load_balancers()
-
-    # Extract load balancer information
-    load_balancers = response['LoadBalancers']
-    for lb in load_balancers:
-        print(f"Load Balancer Name: {lb['LoadBalancerName']}")
-        print(f"Load Balancer ARN: {lb['LoadBalancerArn']}")
-        print(f"Load Balancer DNS Name: {lb['DNSName']}")
-        print(f"Load Balancer Scheme: {lb['Scheme']}")
-        print(f"Load Balancer VPC ID: {lb['VpcId']}")
-        print("\n")"""
+    
     #load_balancer_arn = 'arn:aws:elasticloadbalancing:us-east-1:295397358094:targetgroup/poovatarget/5c5d18dd6c89082d'
-    load_balancer_arn = 'arn:aws:elasticloadbalancing:us-east-1:295397358094:targetgroup/ASGpoo-1/0736934b5d40817f'
-    sns_topic_arn = 'arn:aws:sns:us-east-1:295397358094:sns-poo'
+    alb_name = 'my-alb'
+    asg_name = 'my-asg'
+    #load_balancer_arn = 'arn:aws:elasticloadbalancing:us-east-1:295397358094:targetgroup/ASGpoo-1/0736934b5d40817f'
+    sns_topic_arn = 'arn:aws:sns:us-east-1:471112994703:poo_sns'
+
+
+    alb_describe = elbv2.describe_load_balancers(Names=[alb_name])
+
+    alb_arns = alb_describe['LoadBalancers'][0]['LoadBalancerArn']
+
+    # Get the target group attached to the ALB
+    target_groups = elbv2.describe_target_groups(LoadBalancerArn=alb_arns)
+    target_group_arn = target_groups['TargetGroups'][0]['TargetGroupArn']
+
+    print(target_group_arn)
 
     # Describe instances registered with the Load Balancer
-    instances = elbv2.describe_target_health(TargetGroupArn=load_balancer_arn)
+    instances = elbv2.describe_target_health(TargetGroupArn=target_group_arn)
 
     # Check the health status of each instance
     for instance in instances['TargetHealthDescriptions']:
@@ -38,8 +43,14 @@ def lambda_handler(event, context):
         instance_id = instance['Target']['Id']
         health_status = instance['TargetHealth']['State']
 
-        if health_status != ('healthy' or 'unused'):
+        if health_status != ('healthy'):
             unhealthy_instances.append(instance_id)
+
+            # snapshot 
+            snapshot_id = create_snapshot(ec2, instance_id)
+
+             # Terminate the instance
+            terminate_instance(ec2, asg, asg_name, instance_id)
     if unhealthy_instances:
         sns = boto3.client('sns')
         message = f"Health Status: {health_status}"
@@ -58,3 +69,20 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': 'Load Balancer health check completed.'
     }
+
+
+def create_snapshot(ec2, instance_id):
+    response = ec2.create_snapshot(
+        Description=f"Snapshot for debugging instance {instance_id}",
+        VolumeId=instance_id
+    )
+    return response['SnapshotId']
+
+
+def terminate_instance(ec2, asg, asg_name, instance_id):
+    
+    asg.detach_instances(InstanceIds=[instance_id], AutoScalingGroupName=asg_name, ShouldDecrementDesiredCapacity=True)
+
+    # Terminate the instance
+    ec2.terminate_instances(InstanceIds=[instance_id])
+
